843d7eb14c9d26135a03d83b270065f59353f100
Christian Lehr <christian_lehr.1@gmx.de>
2016-05-01 14:16:17 +0200

    added blog page to site

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,10 @@ class PagesController < ApplicationController
   def portfolio
   end
 
+  def blog
+  	
+  end
+
   def about
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+   
+  resources :articles
 
   root 'pages#welcome'
 
   get '/portfolio', to: 'pages#portfolio'
+
+  get '/blog', to: 'articles#blog'
+
+  get '/blognew', to: 'articles#new'
 
   get '/about', to: 'pages#about'
 
